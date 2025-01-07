02bba8e3c1fdfdb7661b72cc026e88aa0c75a8cc
Abinash Karmi <161156045+Abinash673@users.noreply.github.com>
2025-01-07 22:04:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -15,7 +15,7 @@ export const  connectDB = async () =>{
 
 import mongoose from 'mongoose';
 
-const url = 'mongodb://127.0.0.1:27017/myDrive';
+const url = 'mongodb://localhost:27017/';
 
 export const connectDB = async () => {
     try {
