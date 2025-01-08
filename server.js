9ef722aad77251e0b80735a301188a7dbd385d45
Abinash673 <kumarabinash673@gmail.com>
2025-01-08 11:05:00 +0530

    add url

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-import express  from "express"
-import cors from 'cors'
-import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
-import foodRouter from "./routes/foodRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from 'cors';
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import 'dotenv/config';
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
-const app = express()
+const app = express();
 const port = process.env.PORT || 4000;
-
+const host = process.env.HOST || '127.0.0.1'; // Use a valid hostname or IP address
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors({
+  origin: 'https://food-front-byak.netlify.app/', // Allow requests from your frontend URL
+  credentials: true, // Optional: Allow cookies and authentication headers
+}));
 
 // db connection
-connectDB()
+connectDB();
 
 // api endpoints
-app.use("/api/user", userRouter)
-app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/cart", cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API Working")
-  });
+  res.send("API Working");
+});
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
+// Start the server with the specified hostname and port
+app.listen(port, host, () => {
+  console.log(`Server started on http://${host}:${port}`);
+});
